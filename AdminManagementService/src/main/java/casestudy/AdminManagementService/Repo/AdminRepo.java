f03b8c6f29c15cb5a6af57294be3a2910af3e98b
aritrachatterjee99 <98972277+aritrachatterjee99@users.noreply.github.com>
2022-06-04 01:27:38 +0530

    Delete AdminManagementService directory

--- AdminManagementService/src/main/java/casestudy/AdminManagementService/Repo/AdminRepo.java
+++
@@ -1,9 +0,0 @@
-package casestudy.AdminManagementService.Repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import casestudy.AdminManagementService.model.Flight;
-
-public interface AdminRepo extends MongoRepository<Flight, String> {
-
-}
